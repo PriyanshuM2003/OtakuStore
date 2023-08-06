3955fcd975cc03877738e7521c79fb81f416d6f3
Priyanshu-Butterpaper1 <priyanshu.mishra@butterpaper.tech>
2023-08-06 12:21:59 +0530

    search fixed

--- pages/search.js
+++ pages/search.js
@@ -4,7 +4,7 @@ import Product from "../models/Product"
 import mongoose from "mongoose";
 import Head from 'next/head';
 
-const SearchPage = ({ products,tags }) => {
+const SearchPage = ({ products, tags }) => {
 
     return (
         <>
@@ -14,24 +14,24 @@ const SearchPage = ({ products,tags }) => {
                         <div className="flex flex-wrap -m-4 justify-center">
                             {Object.keys(products).length === 0 && <p>Sorry! All items are currently out of stock.</p>}
                             {Object.keys(products).map((item) => {
-                                return <Link key={products[item]._id} passHref={true} href={`/product/${products[item].slug}`}>
-                                    <div>
-                                        <Head>
-                                            <title>{`OtakuStore- Search=${products[item].tags}`}</title>
-                                            <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0" />
-                                        </Head>
-                                        <h1 className="text-2xl text-center py-4 md:flex md:items-baseline justify-between inline-block border-b border-gray-200 pb-4">Search results for:&nbsp;
-                                            <span className="text-purple-600 font-semibold">
-                                                {tags.map((tag, index) => (
-                                                    <React.Fragment key={index}>
-                                                        {index > 0 && ', '}
-                                                        {`"${tag}"`}
-                                                    </React.Fragment>
-                                                ))}
-                                            </span>
-                                        </h1>
+                                return <div key={products[item]._id}>
+                                    <Head>
+                                        <title>{`OtakuStore- Search=${products[item].tags}`}</title>
+                                        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0" />
+                                    </Head>
+                                    <h1 className="text-2xl text-center py-4 md:flex md:items-baseline justify-between inline-block border-b border-gray-200 pb-4">Search results for:&nbsp;
+                                        <span className="text-purple-600 font-semibold">
+                                            {tags.map((tag, index) => (
+                                                <React.Fragment key={index}>
+                                                    {index > 0 && ', '}
+                                                    {`"${tag}"`}
+                                                </React.Fragment>
+                                            ))}
+                                        </span>
+                                    </h1>
+                                    <Link passHref={true} href={`/product/${products[item].slug}`}>
                                         <div className="lg:w-52 md:w-1/2 p-4
-                                        flex flex-row flex-wrap justify-center py-2 w-full cursor-pointer shadow-lg m-auto mt-6 hover:shadow-xl rounded-lg overflow-hidden">
+                                        flex flex-row flex-wrap justify-center py-2 w-full cursor-pointer hover:shadow-slate-300 shadow-lg m-auto mt-6 hover:shadow-xl rounded-lg overflow-hidden">
                                             <a className="block rounded overflow-hidden">
                                                 <img alt="ecommerce" className="object-cover object-center w-full h-auto block" src={products[item].img} />
                                             </a>
@@ -41,8 +41,8 @@ const SearchPage = ({ products,tags }) => {
                                                 <p className="mt-1 text-purple-800 text-lg font-bold">₹{products[item].price}</p>
                                             </div>
                                         </div>
-                                    </div>
-                                </Link>
+                                    </Link>
+                                </div>
                             })}
                         </div>
                     </div>
